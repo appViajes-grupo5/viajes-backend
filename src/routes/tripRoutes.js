0a741cb72144338937628884befac4796772803a
Kike-ve <manueleortizros@gmail.com>
2025-12-10 23:31:23 +0100

    sustituimos authenticateToken por authMiddleware

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const tripController = require("../controllers/tripController");
-const { authenticateToken } = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 // Rutas públicas
 router.get("/", tripController.getTrips);
 router.get("/:id", tripController.getTrip);
 
 // Rutas protegidas (user autenticado)
-router.post("/", authenticateToken, tripController.createTrip);
-router.put("/:id", authenticateToken, tripController.updateTrip);
-router.delete("/:id", authenticateToken, tripController.deleteTrip);
+router.post("/", authMiddleware, tripController.createTrip);
+router.put("/:id", authMiddleware, tripController.updateTrip);
+router.delete("/:id", authMiddleware, tripController.deleteTrip);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usuarios');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 
 // Ruta para obtener el usuario actual (autenticado)
-router.get('/me', authenticateToken, userController.getCurrentUser);
+router.get('/me', authMiddleware, userController.getCurrentUser);
 
 // Ruta para actualizar el usuario actual (autenticado)
-router.put('/me', authenticateToken, userController.updateUser);
+router.put('/me', authMiddleware, userController.updateUser);
 
 // Ruta para obtener un usuario por ID (público o protegido según necesites)
 router.get('/:id', userController.getUserById);
